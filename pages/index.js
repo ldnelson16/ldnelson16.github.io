646d86467f797e93399605c109043c5d03696dbc
ldnelson16 <ldnelson16@gmail.com>
2023-07-30 13:09:05 -0400

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+//import Head from 'next/head';
+//import Layout, { siteTitle } from '../components/layout';
+//import Navbar from '../components/navbar';
+//import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <section>
+          <p>[Your Self Introduction]</p>
+          <p>
+            (This is a sample website - you’ll be building a site like this on{' '}
+            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          </p>
+        </section>
+      </div>
+    </>
+  );
+}
